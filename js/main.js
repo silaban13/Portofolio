ce674781ac337934893be6575f8b61edf50d8544
Mariono Silaban <silabanmariono91@gmail.com>
2026-01-21 23:09:26 +0700

    memperbaharui content

--- js/main.js
+++ js/main.js
@@ -32,3 +32,40 @@ function initNavbar() {
 
 loadComponent("navbar", "components/Navbar.html");
 loadComponent("footer", "components/Footer.html");
+
+const texts = [
+  "Saya Seorang Mahasiswa yang sering",
+  "Belajar Frontend",
+  "Belajar Backend"
+];
+
+const typingText = document.getElementById("typing-text");
+
+let textIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+  const currentText = texts[textIndex];
+
+  if (!isDeleting) {
+    typingText.textContent = currentText.substring(0, charIndex + 1);
+    charIndex++;
+
+    if (charIndex === currentText.length) {
+      setTimeout(() => isDeleting = true, 1500);
+    }
+  } else {
+    typingText.textContent = currentText.substring(0, charIndex - 1);
+    charIndex--;
+
+    if (charIndex === 0) {
+      isDeleting = false;
+      textIndex = (textIndex + 1) % texts.length;
+    }
+  }
+
+  setTimeout(typeEffect, isDeleting ? 50 : 100);
+}
+
+typeEffect();
